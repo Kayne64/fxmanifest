25c1a805ac1622255090b13da287b5a03b88e9f2
Kayne <93083042+Kayne64@users.noreply.github.com>
2021-10-25 14:54:49 +0800

    add the actual file

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+-- Stating the FX Version and Game 
+fx_version 'cerulean'
+games "gta5"
+
+-- Insert Author Name, Description and Version of Script
+author 'Insert Author Name'
+description 'Insert Description'
+version 'Insert Version'
+
+-- Insert Client Script Names
+client_scripts {
+    'cl.lua',
+    'cl2.lua'
+}
+
+-- Insert Server Script Names
+server_scripts {
+    'sv.lua',
+    'sv2.lua'
+}
